9bf1bfbad0f402b456b6890a0cbb79a9ce42fab5
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-10-12 17:59:14 +0100

    init

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/flask-db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+api = Api(app)  # Initialize Flask-RESTful
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,3 +17,22 @@ class User(db.Model):
     def __init__(self, username, email):
         self.username = username
         self.email = email
+
+# Create a resource for fetching all users
+class UsersResource(Resource):
+    def get(self):
+        users = User.query.all()
+        user_list = []
+        for user in users:
+            user_list.append({
+                'id': user.id,
+                'username': user.username,
+                'email': user.email
+            })
+        return {'users': user_list}
+
+# Add the UsersResource to the API and specify the route
+api.add_resource(UsersResource, '/users')
+
+if __name__ == '__main__':
+    app.run(debug=True)
